0382502a11c039c2689b2d298c850d00d10263ee
Mike Swanson <mikeonthecomputer@gmail.com>
2026-08-17 09:00:41 -0700

    Automated: Update commit IDs and hashes

--- 86box/emulator.scm
+++ 86box/emulator.scm
@@ -38,10 +38,10 @@
   #:use-module (gnu packages vulkan)
   #:use-module (gnu packages xml))
 
-(define %86box-git-commit        "870bf3630e08438cd48d003d33e62de536d4d1e8")
+(define %86box-git-commit        "2cbcb2c2407e7d77d4122d9975504e22b11135f1")
 (define %86box-roms-git-commit   "bd8413e124c580050f71f586675dde1aa4cd7628")
 (define %86box-assets-git-commit "f06840ba5cb7cd3d42f1faa7fe418871a3b3be52")
-(define %86box-git-hash          "1a0hh45s5fq9akmal7h9zfn0yz7i0wj1i5xhq03rybbz83jc94kv")
+(define %86box-git-hash          "1h2x30cp31mhvhv7sxhc5sbv70mfkzq0aaxgfrh0im2lfnwcixrl")
 (define %86box-roms-git-hash     "09v8j08s7mljbfcrx0fkm4fz4mr21xh3ngky6ndl4spl1c7j6003")
 (define %86box-assets-git-hash   "0q1vkr2pf7air5wqzasjkcz40hlj88rlafqr6wvs7662s9ajd3c7")
 
