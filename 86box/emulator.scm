6ab0718829ae1945dc57a9f469b4314326d07e83
Mike Swanson <mikeonthecomputer@gmail.com>
2026-09-26 03:00:37 -0700

    Automated: Update commit IDs and hashes

--- 86box/emulator.scm
+++ 86box/emulator.scm
@@ -38,13 +38,13 @@
   #:use-module (gnu packages vulkan)
   #:use-module (gnu packages xml))
 
-(define %86box-git-date          "20260926083847")
+(define %86box-git-date          "20260926094853")
 (define %86box-roms-git-date     "20260925023120")
 (define %86box-assets-git-date   "20260904181348")
-(define %86box-git-commit        "75f342d23c7fc6ff51ef7ccc2a9d4f7cfdfd59cd")
+(define %86box-git-commit        "d60bc8e1e02cf0613b07a873399231b31a01ff3d")
 (define %86box-roms-git-commit   "c761288ecddccb36d5664d33c0199e9d0b4e8454")
 (define %86box-assets-git-commit "6b23b7c03732049e5d979622b49b8d87061d3e32")
-(define %86box-git-hash          "0xnwpqickcgbzrb8ra8nin4wl1kc20fykqvxq8r23ddg5v3dvjs9")
+(define %86box-git-hash          "0l6hvgbidx1a1mclp9a88iyx9zgf77bfq7vhi8j9k4fk1dp4h70k")
 (define %86box-roms-git-hash     "15an0l9yawvnxi6qwp6zqzrb15lv2nj44ngy36vmkifbw8jqm7ss")
 (define %86box-assets-git-hash   "01f1vl1vw8snpbqss6knc9kk4all1g93l86qzq7sh49rmaav8n31")
 
